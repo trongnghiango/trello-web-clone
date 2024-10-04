e0ea9babf3ec40c00bedbc4b1c4cf937fc2f6b6c
Ka ThinkLap <trongnghia.ngo@hotmail.com>
2024-10-04 11:51:04 +0700

    config mui theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from './theme'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
     <CssBaseline />
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
